26b43be51fee4a237e6105d7773017c7b79b961c
Sandor Kaman <kaman.sandor@gmail.com>
2016-11-27 15:23:30 +0100

    Progress status indication is moved to CLI.

--- CLI/deployer.rb
+++ CLI/deployer.rb
@@ -7,32 +7,38 @@ require 'main'
 class DrupalCLI < Thor
   def initialize(*args)
     super
+    @main = Main.new
   end
 
   option :stackname, required: true
   option :config, required: false
   desc 'create', 'create AWS stack'
   def create
-  	@main = Main.new
+    credentials = read_config_file(options[:config])
     @main.create_stack(options[:stackname])
+    print_status(@main.aws_client, 'CREATE_IN_PROGRESS', options[:stackname],
+                 credentials)
   end
 
   option :stackname, required: true
   option :config, required: false
   desc 'delete', 'delete AWS stack'
   def delete
-  	@main = Main.new
+    credentials = read_config_file(options[:config])
     @main.delete_stack(options[:stackname])
+    print_status(@main.aws_client, 'DELETE_IN_PROGRESS', options[:stackname],
+                 credentials)
+  rescue Aws::CloudFormation::Errors::ValidationError
+    puts "#{stackname} is deleted"
   end
 
   desc 'check', 'check if drupal is' \
-  		'reachable on the defined ip and port'
+  		' reachable on the defined ip and port'
   option :server, required: true
   def check
     status, body = @main.check_if_drupal_is_up(options[:server])
     if status
       puts 'Drupal is up!'
-      puts body
     else
       puts 'Drupal is down!'
     end
@@ -50,8 +56,28 @@ class DrupalCLI < Thor
   end
 end
 
+def print_status(client, condition, stackname, credentials)
+  Whirly.configure spinner: 'dots'
+  Whirly.start
+  Whirly.status = whirly_status(condition, stackname)
+  current_status = client.check_stack_status(credentials)
+  while current_status == condition
+    sleep(5)
+    current_status = client.check_stack_status(credentials)
+  end
+  Whirly.stop
+  puts client.check_stack_status(credentials)
+end
+
+def whirly_status(condition, stackname)
+  case condition
+  when 'DELETE_IN_PROGRESS' then "Deleting #{stackname}"
+  when 'CREATE_IN_PROGRESS' then "Creating #{stackname}"
+  end
+end
+
 begin
-	DrupalCLI.start(ARGV)
+  DrupalCLI.start(ARGV)
 rescue Errno::ENOENT
-	puts "No config file was found in #{__dir__}"
+  puts "No config file was found in #{__dir__}"
 end
--- CLI/main.rb
+++ CLI/main.rb
@@ -7,32 +7,24 @@ require 'aws_cred.rb'
 
 # Class which contains main functionalities of the program
 class Main
-  def initialize(config)
-    read_config(config)
-  end
-
   def create_stack(stackname)
-    aws_client = AWSClient.new(@credentials, stackname)
-    aws_client.create_stack
-    print_status(aws_client, 'CREATE_IN_PROGRESS', stackname)
+    @aws_client = AWSClient.new(@credentials, stackname)
+    @aws_client.create_stack
   end
 
   def delete_stack(stackname)
-    aws_client = AWSClient.new(@credentials, stackname)
-    aws_client.delete_stack
-    print_status(aws_client, 'DELETE_IN_PROGRESS', stackname)
-  rescue Aws::CloudFormation::Errors::ValidationError
-    puts "#{stackname} is deleted"
+    @aws_client = AWSClient.new(@credentials, stackname)
+    @aws_client.delete_stack
   end
 
   def check_if_drupal_is_up(server)
     address = 'http://' + server + '/example'
     begin
       open(address) do |content|
-        return analyse_content(content)
+        validate_response(content)
       end
     rescue OpenURI::HTTPError, Errno::ETIMEDOUT, SocketError
-      [false]
+      false
     end
   end
 
@@ -44,30 +36,11 @@ class Main
                                template: config['cloudformation']['template'])
   end
 
+  attr_reader :aws_client
+
   private
 
-  def print_status(client, condition, stackname)
-    Whirly.configure spinner: 'dots'
-    Whirly.start
-    Whirly.status = whirly_status(condition, stackname)
-    current_status = client.check_stack_status(@credentials)
-    while current_status == condition
-      sleep(5)
-      current_status = client.check_stack_status(@credentials)
-    end
-    Whirly.stop
-    puts client.check_stack_status(@credentials)
-  end
-
-  def whirly_status(condition, stackname)
-    return "Deleting #{stackname}" if condition == 'DELETE_IN_PROGRESS'
-    return "Creating #{stackname}" if condition == 'CREATE_IN_PROGRESS'
-  end
-
-  def analyse_content(content)
-    body = content.read
-    to_return = [true, body.match(%r{<head>.*</head>}m)]
-    return to_return if content.status[0] == '200' && body.include?('Drupal')
-    [false]
+  def validate_response(content)
+    content.status[0] == '200' && content.read.include?('Drupal')
   end
 end
